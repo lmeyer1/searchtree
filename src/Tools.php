0a9fbf0e0c63ec468cb43e9c7431c78c267739c7
Lorenz Meyer <lorenz@meyer.tl>
2023-09-18 12:30:23 +0200

    implement bit_substr with tests

--- src/Tools.php
+++ src/Tools.php
@@ -4,25 +4,61 @@ namespace Searchindex;
 
 class Tools
 {
-	/**
-	 * @param length int between 0 and 24 (3 bytes)
-	 */
+	public const BIN_DUMP_DEC = 0;
+	public const BIN_DUMP_HEX = 1;
+	public const BIN_DUMP_BIN = 2;
+
 	public static function bit_substr($string, $offset, $length)
 	{
-		$substr = substr($string, intdiv($offset, 8), intdiv($length + 7, 8));
+		$substr = substr($string, intdiv($offset, 8), intdiv($length + 7 + $offset % 8, 8));
+		if ($offset % 8) { // shift the bits
+			$substr_new = '';
+			$retain = "\0";
+			for($i = 0; $i < strlen($substr) / 3; $i++) {
+				$int32 = unpack('N', str_pad(substr($substr, $i * 3, 3), 4, "\0", STR_PAD_LEFT));
+				$str4 = pack('N', $int32[1] << ($offset % 8));
+				$bytes_exceeding = ($i + 1) * 3 - strlen($substr);
+				$bytes_exceeding = $bytes_exceeding > 0 ? $bytes_exceeding : 0;
+				$shifted = substr($str4, 1 + $bytes_exceeding);
+				if(ord($retain)) {
+					$substr_new[-1] = chr(ord($substr_new[-1]) | ord($retain));
+				}
+				$substr_new .= $shifted;
+				$retain = substr($str4, 0, 1);
+			}
+			$substr = substr($substr_new, 0, intdiv($length + 7, 8));
+		}
+		if ($length % 8) {
+			$masks = [0x80, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc, 0xfe, 0xff];
+			$substr[-1] = chr(ord($substr[-1]) & $masks[$length % 8 - 1]);
+		}
 		return $substr;
 	}
 	
-	public static function bin_dump($string)
+	public static function bin_dump($string, $mode = self::BIN_DUMP_HEX, $return = false)
 	{
-		$dec = $hex = $bin = '';
+		$output = '';
 		for($i = 0;$i < strlen($string); $i++) {
-			$dec += ord($string[$i]) + ' ';
-			$hex += str_pad(base_convert(ord($string[$i]), 10, 16), 2, '0', STR_PAD_LEFT) + ' ';
-			$bin += str_pad(base_convert(ord($string[$i]), 10, 2), 8, '0', STR_PAD_LEFT) + ' ';
+			switch ($mode) {
+				case self::BIN_DUMP_DEC:
+					$output .= ord($string[$i]);
+					break;
+				case self::BIN_DUMP_HEX:
+					$output .= str_pad(base_convert(ord($string[$i]), 10, 16), 2, '0', STR_PAD_LEFT);
+					break;
+				case self::BIN_DUMP_BIN:
+					$output .= str_pad(base_convert(ord($string[$i]), 10, 2), 8, '0', STR_PAD_LEFT);
+			}
+			$output .= ' ';
+			if ($i && !($i % 8)) {
+				$output .= PHP_EOL;
+			}
 		}
-		echo $dec, PHP_EOL;
-		echo $hex, PHP_EOL;
-		echo $bin, PHP_EOL;
+		if ($return) {
+			return trim($output);
+		} else {
+			echo trim($output) . PHP_EOL;
+			return;
+		}
 	}
 }	
--- test/ToolsTest.php
+++ test/ToolsTest.php
@@ -1,14 +1,51 @@
 <?php declare(strict_types=1);
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Searchindex\Tools;
 
 final class ToolsTest extends TestCase
 {
-    public function testBitSubstr(): void
+	public static function bitSubstrProvider(): array
+	{
+		return [
+            ['abc', 0, 24, 'abc'],
+            ['abc', 0, 12, 'a`'],
+            ['abc', 1, 2, "\xc0"],
+            ['abc', 8, 15, 'bb'],
+            ['abc', 6, 16, "X\x98"],
+            ['abcdefg', 3, 47, "\x0b\x13\x1b\x23\x2b\x32"],
+        ];
+	}
+
+	/**
+	 * @dataProvider bitSubstrProvider
+	 */
+    public function testBitSubstr(string $input, int $offset, int $length, string $result): void
     {
+		$substr = Tools::bit_substr($input, $offset, $length);
         $this->assertEquals(
-            'abc',
-            Tools::bit_substr('abc', 0, 24)
+            $result,
+            $substr
         );
     }
+
+	public static function binDumpProvider(): array
+	{
+		return [
+            ['abc', 0, '97 98 99'],
+            ['abc', 1, '61 62 63'],
+            ['abc', 2, '01100001 01100010 01100011'],
+        ];
+	}
+
+	/**
+	 * @dataProvider binDumpProvider
+	 */
+	public function testBinDump(string $input, int $mode, string $result): void
+	{
+		$this->assertEquals(
+            $result,
+            Tools::bin_dump($input, $mode, true)
+        );
+	}
 }
